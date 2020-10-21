66ea765f5c1b2f2c5ee5c381fd3f6ef10a7fed23
Jake Nemiroff <jakenemiroff@gmail.com>
2020-10-20 22:44:39 -0400

    add function for building assimilators and make code more readable

--- StarCraftBot.py
+++ StarCraftBot.py
@@ -1,7 +1,7 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
 
 class SCBot(sc2.BotAI):
 
@@ -11,11 +11,13 @@ class SCBot(sc2.BotAI):
         await self.distribute_workers()
         await self.build_workers()
         await self.build_pylons()
+        await self.build_assimilators()
 
-        # code responsible for building worker units
+    # code responsible for building worker units
     async def build_workers(self):
 
         for nexus in self.units(NEXUS).ready.noqueue:
+
             if self.can_afford(PROBE):
                 await self.do(nexus.train(PROBE))
 
@@ -25,9 +27,33 @@ class SCBot(sc2.BotAI):
 
         if self.supply_left < 5 and not self.already_pending(PYLON):
             nexuses = self.units(NEXUS).ready
+
             if nexuses.exists:
+
                 if self.can_afford(PYLON):
                     await self.build(PYLON, near=nexuses.first)
+
+    # function for constructing assimilators, a structure required to gather
+    # one in game resource
+    async def build_assimilators(self):
+
+        for nexus in self.units(NEXUS).ready:
+
+            geysers = self.state.vespene_geyser.closer_than(10.0, nexus)
+
+            for geyser in geysers:
+
+                if not self.can_afford(ASSIMILATOR):
+                    break
+
+                worker = self.select_build_worker(geyser.position)
+
+                if worker is None:
+                    break
+
+                if not self.units(ASSIMILATOR).closer_than(1.0, geyser).exists:
+                    await self.do(worker.build(ASSIMILATOR, vaspene))
+
 
 def main():
     sc2.run_game(
