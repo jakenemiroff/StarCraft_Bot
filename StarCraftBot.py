f5796b6c55ddbf43a92f8b6f5eb7164927b5136b
Jake Nemiroff <jakenemiroff@gmail.com>
2020-10-22 21:13:05 -0400

    make slight modifications, able to consistently beat hard AI

--- StarCraftBot.py
+++ StarCraftBot.py
@@ -8,7 +8,6 @@ class SCBot(sc2.BotAI):
 
     def __init__(self):
         self.ITERATIONS_PER_MINUTE = 165
-        self.WORKER_CAP = 50
 
     # code executed on every step
     async def on_step(self, iteration):
@@ -20,15 +19,13 @@ class SCBot(sc2.BotAI):
         await self.expand()
         await self.build_offensive_buildings()
         await self.train_units()
-        await self.defend()
         await self.attack()
 
     # code responsible for building worker units
     async def build_workers(self):
 
-        if len(self.units(PROBE)) < self.WORKER_CAP:
-            for nexus in self.units(NEXUS).ready.noqueue:
-
+        for nexus in self.units(NEXUS).ready.noqueue:
+            if self.workers.amount < self.units(NEXUS).amount * 15 and nexus.noqueue:
                 if self.can_afford(PROBE):
                     await self.do(nexus.train(PROBE))
 
@@ -69,11 +66,15 @@ class SCBot(sc2.BotAI):
     # necessary for acquiring additional resources
     async def expand(self):
 
-        if self.units(NEXUS).amount < 2 and self.can_afford(NEXUS):
-            await self.expand_now()
+        if self.units(NEXUS).amount < 3 and not self.already_pending(NEXUS) and len(self.units(NEXUS)) < ((self.iteration / self.ITERATIONS_PER_MINUTE) / 2.5):
+            if self.can_afford(NEXUS):
+                await self.expand_now()
 
     # function for building offensive buildings
     async def build_offensive_buildings(self):
+
+        map_center = self.game_info.map_center
+        position_towards_map_center = self.start_location.towards(map_center, distance = 8)
 
         if self.units(PYLON).ready.exists:
 
@@ -81,58 +82,56 @@ class SCBot(sc2.BotAI):
 
             if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
                     if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
-                        await self.build(CYBERNETICSCORE, near = pylon)
+                        await self.build(CYBERNETICSCORE, near = position_towards_map_center, placement_step = 1)
 
-            elif len(self.units(GATEWAY)) < (self.iteration / self.ITERATIONS_PER_MINUTE):
+            elif len(self.units(GATEWAY)) < (self.iteration / self.ITERATIONS_PER_MINUTE) / 3:
                 if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
                     await self.build(GATEWAY, near=pylon)
 
             if self.units(CYBERNETICSCORE).ready.exists:
 
-                if len(self.units(STARGATE)) < ((self.iteration / self.ITERATIONS_PER_MINUTE) / 2):
+                if len(self.units(STARGATE)) < ((self.iteration / self.ITERATIONS_PER_MINUTE) / 5) and len(self.units(STARGATE)) < 4:
                     if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
                         await self.build(STARGATE, near = pylon)
 
-                if len(self.units(ROBOTICSFACILITY)) < ((self.iteration / self.ITERATIONS_PER_MINUTE) / 2):
+                if len(self.units(ROBOTICSFACILITY)) < ((self.iteration / self.ITERATIONS_PER_MINUTE) / 5) and len(self.units(ROBOTICSFACILITY)) < 4:
                     if self.can_afford(ROBOTICSFACILITY) and not self.already_pending(ROBOTICSFACILITY):
                         await self.build(ROBOTICSFACILITY, near = pylon)
 
     # function for handling the creation of offensive units
     async def train_units(self):
 
-        for gateway in self.units(GATEWAY).ready.noqueue:
-
-            if not self.units(STALKER).amount > self.units(VOIDRAY).amount:
-                if self.can_afford(STALKER) and self.supply_left > 0:
-                    await self.do(gateway.train(STALKER))
-
-            elif self.units(STALKER).amount > self.units(ZEALOT).amount:
-                if self.can_afford(ZEALOT) and self.supply_left > 0:
-                    await self.do(gateway.train(ZEALOT))
-
         for starGate in self.units(STARGATE).ready.noqueue:
+
             if self.can_afford(VOIDRAY) and self.supply_left > 0:
                 await self.do(starGate.train(VOIDRAY))
 
         for roboticsFacility in self.units(ROBOTICSFACILITY).ready.noqueue:
+
             if self.can_afford(IMMORTAL) and self.supply_left > 0:
                 await self.do(roboticsFacility.train(IMMORTAL))
 
+        for gateway in self.units(GATEWAY).ready.noqueue:
 
-    async def defend(self):
+            if self.can_afford(STALKER) and self.supply_left > 0:
+                await self.do(gateway.train(STALKER))
 
-        if self.units(STALKER).amount > 5:
-            if len(self.known_enemy_units) > 0:
+            elif self.can_afford(ZEALOT) and self.supply_left > 0:
+                await self.do(gateway.train(ZEALOT))
 
-                for stalker in self.units(STALKER).idle:
-                    await self.do(stalker.attack(random.choice(self.known_enemy_units)))
 
     async def attack(self):
 
-        if self.units(STALKER).amount > 20:
+        units = [ZEALOT, STALKER, VOIDRAY, IMMORTAL, COLOSSUS]
 
-            for stalker in self.units(STALKER).idle:
-                await self.do(stalker.attack(self.scout(self.state)))
+        for unit in units:
+            if self.army_count > 35:
+                for specificUnit in self.units(unit).idle:
+                    await self.do(specificUnit.attack(self.scout(self.state)))
+
+            elif len(self.known_enemy_units) > 0:
+                    for specificUnit in self.units(unit).idle:
+                        await self.do(specificUnit.attack(random.choice(self.known_enemy_units)))
 
     def scout(self, state):
 
@@ -143,12 +142,12 @@ class SCBot(sc2.BotAI):
             return random.choice(self.known_enemy_structures)
 
         else:
-            return self.enemy_start_locations[0]
+            return random.choice(self.enemy_start_locations)
 
 def main():
     sc2.run_game(
         sc2.maps.get("AbyssalReefLE"),
-        [Bot(Race.Protoss, SCBot()), Computer(Race.Terran, Difficulty.Easy)],
+        [Bot(Race.Protoss, SCBot()), Computer(Race.Zerg, Difficulty.Hard)],
         realtime=False,
     )
 
